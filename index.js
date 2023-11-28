097fb0c1767b5d606f10784a48341e13835bdf88
mansur <mansur_dzhamaldinov.@mail.ru>
2023-11-29 00:30:22 +0300

    start

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const cors = require("cors")
+const path = require("path")
+const mongoose = require("mongoose")
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/images", express.static(path.resolve(__dirname, "images")))
+
+// routes
+
+//
+
+mongoose.connect("mongodb+srv://mitkorol90:mitkorol90@cluster0.zpgt7p8.mongodb.net/Project-first-week")
+.then(() => console.log('ok'))
+.catch(() => console.log('error'))
+
+app.listen(3040, () => console.log("Сервер запущен!"))
