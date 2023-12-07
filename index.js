78bae4ea3570042a5ad143e2529244b46f6d3ad9
mansur <mansur_dzhamaldinov.@mail.ru>
2023-12-07 15:18:36 +0300

    online chat

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 require("dotenv").config()
-const cors = require("cors")
+const cors = require('cors')
 const path = require("path")
 const mongoose = require("mongoose")
 const express = require("express")
 
 const app = express()
 
+const http = require("http").Server(app)
+
 app.use(express.json())
 app.use(cors())
-
 app.use("/images", express.static(path.resolve(__dirname, "images")))
 
+const socketIO = require("socket.io")(http, {
+    cors : {
+        origin: "http://localhost:5173"
+    }
+})
+
+app.get("/api", (req,res) => {
+    res.json( {
+        message: "Hello"
+    })
+})
+
+const users = []
+
+socketIO.on("connection", (socket) => { // я хз как но заработало и он почему каждый раз новый айдишник генит
+    console.log(`${socket.id} user connected`); // Достает айди юзера
+    socket.on("message", (data) => {
+        socketIO.emit("response", data)
+        console.log("Message",data);
+    })
+    socket.on("typing", (data) => socket.broadcast.emit("responseTyping", data))
+    socket.on("newUser", (data) => {
+        users.push(data)
+        socketIO.emit("responseNewUser", users)
+    })
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} disconnect`);
+    })
+})
 // routes
 app.use(require("./routes/users.route"))
 app.use(require("./routes/comments.route"))
@@ -21,4 +51,4 @@ mongoose.connect("mongodb+srv://mitkorol90:mitkorol90@cluster0.zpgt7p8.mongodb.n
 .then(() => console.log('ok'))
 .catch(() => console.log('error'))
 
-app.listen(3030, () => console.log("Сервер запущен!"))
+http.listen(3030, () => console.log("Сервер запущен!"))
